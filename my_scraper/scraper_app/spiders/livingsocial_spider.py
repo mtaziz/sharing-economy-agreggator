230b42a07f8a372c33fbefcdb72917326c4e8fd8
mohamed <mohamed.ibnalkadi@gmail.com>
2015-02-04 17:36:12 +0100

    running the spider for living_social.

--- my_scraper/scraper_app/spiders/livingsocial_spider.py
+++ my_scraper/scraper_app/spiders/livingsocial_spider.py
@@ -1,38 +1,61 @@
+#! -*- coding: utf-8 -*-
+
+"""
+Web Scraper Project
+
+Scrape data from a regularly updated website livingsocial.com and
+save to a database (postgres).
+
+Scrapy spider part - it actually performs scraping.
+"""
+
 from scrapy.spider import BaseSpider
-from scapy.selector import HtmlXPathSelector
+from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.loader import XPathItemLoader
 from scrapy.contrib.loader.processor import Join, MapCompose
 
-from scrapy_app.items import LivingSocialDeal
+from scraper_app.items import LivingSocialDeal
+
 
 class LivingSocialSpider(BaseSpider):
-	""" Spider for regularly updated livingsocial.com site, San Francisco Page"""
-	name = "livingsocial"
-	allowed_domains = ["livingsocial.com"]
-	start_urls = ["http://www.livingsocial.com/cities/15-san-francisco"]
+    """
+    Spider for regularly updated livingsocial.com site, San Francisco page
+    """
+    name = "livingsocial"
+    allowed_domains = ["livingsocial.com"]
+    start_urls = ["https://www.livingsocial.com/cities/15-san-francisco"]
 
-	deals_list_xpath = '//li[@dealid]'
-	item_fields = {
-		'title': './/span[@itemscope]/meta[@itemprop="name"]/@content',
-		'link': './/a/@href',
-		'location': './/a/div[@class="deal-details"]/p[@class="location"]/text()',
-		'original_price': './/a/div[@class="deal-prices"]/div[@class="deal-strikethrough-price"]/div[@class="strikethrough-wrapper"]/text()',
-		'price': './/a/div[@class="deal-prices"]/div[@class="deal-price"]/text()',
-		'end_date': './/span[@itemscope]/meta[@itemprop="availabilityEnds"]/@content'
-		}
+    deals_list_xpath = '//li[@dealid]'
+    item_fields = {
+        'title': './/span[@itemscope]/meta[@itemprop="name"]/@content',
+        'link': './/a/@href',
+        'location': './/a/div[@class="deal-details"]/p[@class="location"]/text()',
+        'original_price': './/a/div[@class="deal-prices"]/div[@class="deal-strikethrough-price"]/div[@class="strikethrough-wrapper"]/text()',
+        'price': './/a/div[@class="deal-prices"]/div[@class="deal-price"]/text()',
+        'end_date': './/span[@itemscope]/meta[@itemprop="availabilityEnds"]/@content'
+    }
 
-	def parse(self, response):
+    def parse(self, response):
+        """
+        Default callback used by Scrapy to process downloaded responses
 
-		selector = HtmlXPathSelector(response)
+        Testing contracts:
+        @url http://www.livingsocial.com/cities/15-san-francisco
+        @returns items 1
+        @scrapes title link
 
-		#iterate over deals
-		for deal in selector.selct(self.deals_list_xpath):
-			loader = XPathItemLoader(LivingSocialDeal(), selector=deal)
+        """
+        selector = HtmlXPathSelector(response)
 
-			# define processors
-			loader.default_input_processor = MapCompose(unicode.strip)
-			loader.default_output_processor = Join()
+        # iterate over deals
+        for deal in selector.xpath(self.deals_list_xpath):
+            loader = XPathItemLoader(LivingSocialDeal(), selector=deal)
 
-			for field, xpath in self.item_fields.iteritems():
-				loader.add_xpath(field, xpath)
-			yield loader.load_item()
+            # define processors
+            loader.default_input_processor = MapCompose(unicode.strip)
+            loader.default_output_processor = Join()
+
+            # iterate over fields and add xpaths to the loader
+            for field, xpath in self.item_fields.iteritems():
+                loader.add_xpath(field, xpath)
+            yield loader.load_item()
