de79ee2cce30c420f134ec5e13c1e9e225d761c7
mohamed <mohamed.ibnalkadi@gmail.com>
2015-03-03 14:35:05 +0100

    sailsharing: spider correction.

--- robot/robot/spiders/sailsharing.py
+++ robot/robot/spiders/sailsharing.py
@@ -14,6 +14,7 @@ class SailsharingSpider(scrapy.Spider):
     def parse(self, response):
         for sel in response.xpath('//div[@class="block"]'):
             item = AdItem()
+            empty = 'unknown'
             try:
                 item['source'] = self.name
             except:
@@ -22,47 +23,42 @@ class SailsharingSpider(scrapy.Spider):
             try:
                 item['category'] = self.category
             except:
-                print("scraping fails")
+                item['category'] = empty
 
             try:
-                item['title'] = sel.xpath('div/h2/a/text()').extract()[0]
+                item['title'] = sel.xpath('div/h2/a/text()').extract()[0].strip("\n ")
+
             except:
-                print("scraping fails")
+                item['title'] = empty
             try:
-                item['media'] = sel.xpath('div/a/img/@src').extract()[0]
+                item['media'] = sel.xpath('a/img/@src').extract()[0]
             except:
-                print("scraping fails")
+                item['media'] = empty
             try:
                 item['url'] = sel.xpath('a/@href').extract()[0]
             except:
-                print("scraping fails")
+                item['url'] = empty
             try:
                 item['description'] = sel.xpath('div/div[@class="boat-info"]/text()').extract()[0].strip("\n ")
 
             except:
-                print("scraping fails")
+                item['description'] = empty
             try:
                 item['location'] = sel.xpath('div/div/h4/strong/text()').extract()[0]
             except:
-                print("scraping fails")
-            
-            try:
-                geolocator = Nominatim()
-                location = geolocator.geocode(item['location'])
-            
-                item['latitude'] = location.latitude
-                item['longitude'] = location.longitude
-            except:
-                print("geocoding not working")
+                item['location'] = empty
+                        
+            item['latitude'] = empty
+            item['longitude'] = empty
             
             try:
                 item['price'] = sel.xpath('div[@class="hosting-meta"]/div/span/strong/text()').extract()[0]
             except:
-                print("scraping fails")
+                item['price'] = empty
             
             try:
                 item['period'] = sel.xpath('div[3]/span/text()').extract()[0]
             except:
-                print("scraping fails")
+                item['period'] = empty
 
             yield item
