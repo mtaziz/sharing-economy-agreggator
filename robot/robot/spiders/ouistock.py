1c9f6d4bdec33217eafcaff91809374f94d6ec92
mohamed <mohamed.ibnalkadi@gmail.com>
2015-03-06 19:41:24 +0100

    ouistock spider correction.

--- robot/robot/spiders/ouistock.py
+++ robot/robot/spiders/ouistock.py
@@ -1,3 +1,4 @@
+#-*- coding:utf8 -*-
 import scrapy 
 from robot.items import AdItem
 import datetime
@@ -6,9 +7,14 @@ from geopy.geocoders import Nominatim
 class OuistockSpider(scrapy.Spider):
 	name = "ouistock"
 	category = "storing"
+	subcategory ="space"
 	allowed_domains = ["https://www.ouistock.fr"]
-	# scrap zilok by categories
-	cities = ['paris--france', 'nantes--france', 'lille--france', 'bordeaux--france', 'nancy--france', 'nice--france']
+	# scrap by cities
+	cities  = [
+		"Paris","Amiens","Nancy",
+		"Rouen","Caen","Evreux","Saint Lo","Rennes","Quimper","Morlaix","Vannes","Strasbourg","Nantes","Clermont Ferrand","Bordeaux","Dax","Chambery",
+		"Poitiers","Perpignan","Nimes","Montpellier","Marseille","Nice","Lyon","Toulouse","Limoges","Besancon","Troyes","Orléans","Le mans","Gap","Millau","Brives"
+	]
 	start_urls_0 = list(map(lambda x: "https://www.ouistock.fr/s/"+str(x), cities))
 	start_urls = [url+"?page="+str(x) for url in start_urls_0 for x in range(100)]
 	
@@ -19,38 +25,48 @@ class OuistockSpider(scrapy.Spider):
 			empty = 'unknown'
 			item['source'] = self.name
 			item['category'] = self.category
+			item['subcategory'] = self.subcategory
+
 			try:
-				item['title'] = sel.xpath("div/div[@class='resultInfos']/span[@class='resultType']/text()").extract()[0].strip('\n ')
+				item['title'] = sel.xpath('div[@class="resultContainer"]/div[@class="resultInfos"]/span[@class="resultUserName"]/text()').extract()[0].strip('\n ')
 
 			except: 
 				item['title'] = empty
 
 			try:	
-				item['media'] = sel.xpath('div/div[@class="resultImgContainer"]/img/@src').extract()[0]
+				item['media'] = sel.xpath('div[@class="resultContainer"]/div[@class="resultImgContainer"]/img/@src').extract()[0]
 			except: 
 				item['media'] = empty
 
 			try:
-				item['url'] = sel.xpath('div/a/@href').extract()[0]
+				item['url'] = sel.xpath('div[@class="resultContainer"]/a/@href').extract()[0]
 			except:
 				item['url'] = empty
 			
 			try:		
-				item['description'] = sel.xpath('div/div[@class="resultInfos"]/span[@class="resultUsefull"]/text()').extract()[0]
+				desc0 = sel.xpath('div[@class="resultContainer"]/div[@class="resultInfos"]/span[@class="resultType"]/text()').extract()[0].strip('\n ')
+				desc1 = sel.xpath('div[@class="resultContainer"]/div[@class="resultInfos"]/span[@class="resultUsefull"]/text()').extract()[0].strip('\n ')
+
+				item['description'] = desc0 + " "+ desc1
 			except:
 				item['description'] = empty
 
-			item['location'] = empty
-			item['latitude'] = location.latitude
-			item['longitude'] = location.longitude
+			try:
+				item['location'] = sel.xpath('div[@class="resultContainer"]/div[@class="resultInfos"]/span[@class="resultUsefull"]/text()').extract()[0].strip('\n ').split(' ')[-1]
+
+			except:
+				item['location'] = empty
+				
+			item['latitude'] = empty
+			item['longitude'] = empty
 
 			try:
-				item['price'] = sel.xpath('div/div[@class="priceSpan"]/div/i/text()"').extract()[0]
+				item['price'] = sel.xpath('div[@class="resultContainer"]/div[@class="priceSpan"]/div[@class="innerSpan"]/i/text()').extract()[0].strip('\n ')
 			except:
 				item['price'] = empty
 
 			try:
-				item['period'] = sel.xpath("div/div[@class='priceSpan']/div/span/text()").extract()[0].strip("\n' /")
+				item['period'] = sel.xpath('div[@class="resultContainer"]/div[@class="priceSpan"]/div[@class="innerSpan"]/i/text()'').extract()[0].strip("\n' /")
 
 			except:
 				item['period'] = empty
