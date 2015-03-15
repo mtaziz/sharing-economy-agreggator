c3e44fc121d3f570c35ee806b63cd1cd342935cc
mohamed <mohamed.ibnalkadi@gmail.com>
2015-03-15 20:19:47 +0100

    robot: zilok scraped.

---
+++ robot/robot/spiders/zilok.py
@@ -0,0 +1,49 @@
+#-*- encoding:utf8 -*-
+import scrapy 
+from robot.items import AdItem
+
+class ZilokSpider(scrapy.Spider):
+	name = "zilok"
+	category = "daily"
+	subcategory = "brico"
+	allowed_domains = ["http://www.zilok.fr"]
+	# scrap boaterfly by pages
+	start_urls = ["http://fr.zilok.com/apiv2/index.php/item/search/api/?action=item.search&api_key=akaka12JHKLAs455saasasa54sJLJLA&distance=2000000&&language=2&lat=48.857&limit=1000&lng=2.351&order=item_rating_number&real_search=1&offset=60"]
+
+
+	def parse(self, response):
+		for sel in response.xpath('//item[@id]'):
+			item = AdItem()
+			item['source'] = self.name
+			item['category'] = self.category
+			item['subcategory'] = self.subcategory
+		 	empty = "unknown"
+		 	try:
+				item['title'] = sel.xpath('title/text()').extract()[0]
+			except:
+				item['title'] = empty
+			try:
+				item['media'] = sel.xpath('image/nail/@url"').extract()[0]
+			except:
+				item['media'] = empty
+			try:
+				item['url'] = sel.xpath('link_book/text()').extract()[0]
+			except:
+				item['url'] = empty
+			try:
+				item['description'] = sel.xpath('subtitle/text()').extract()[0]
+			except:
+				item['description'] = empty
+			try:
+				item['location'] = sel.xpath('location/locality/text()').extract()[0]
+			except:
+				item['location'] = empty
+			item['latitude'] = empty
+			item['longitude'] = empty
+			try:
+				item['price'] = sel.xpath('price/text()').extract()[0].encode('utf-8').strip('€')
+			except:
+				item['price'] = empty
+
+			item['period'] = empty
+			yield item
