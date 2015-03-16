092ce51da4c0c7e1e1d0e0f85266a7aa2564a1a4
mohamed <mohamed.ibnalkadi@gmail.com>
2015-03-16 16:17:36 +0100

    scrape.sh corrected.

--- robot/scrape.sh
+++ robot/scrape.sh
@@ -2,6 +2,8 @@ source $WORKON_HOME/scrapper/bin/activate
 cd /home/mohamed/side_projects/alterre.org/robot
 scrapy crawl drivy 
 scrapy crawl housetrip
+scrapy crawl chambrealouer
+scrapy crawl jelouemoncampingcar
 scrapy crawl boaterfly
 scrapy crawl jestocke
 scrapy crawl ouicar
@@ -12,4 +14,4 @@ scrapy crawl elouesport
 scrapy crawl elouemoto
 scrapy crawl supermarmite
 scrapy crawl meetup
-scrapy crawl zilok
+scrapy crwal zilok
