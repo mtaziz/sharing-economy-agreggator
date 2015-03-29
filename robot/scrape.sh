88953794f87aa08e015e4a28938b88911be7f39a
mohamed <mohamed.ibnalkadi@gmail.com>
2015-03-29 10:32:22 +0200

    robot: eloue hebergement scraping.

--- robot/scrape.sh
+++ robot/scrape.sh
@@ -14,11 +14,13 @@ scrapy crawl ouistock
 scrapy crawl eloue
 scrapy crawl elouesport
 scrapy crawl elouemoto
-scrapy crawl supermarmite
-scrapy crawl meetup
-scrapy crawl zilok
-scrapy crawl airbnb
-scrapy crawl wimdu
-scrapy crawl sejourning
-
+scrapy crawl supermarmite -o supermarmite_`date +%Y-%m-%d-%H-%M`.json
+scrapy crawl meetup -o meetup_`date +%Y-%m-%d-%H-%M`.json
+scrapy crawl zilok -o zilok_`date +%Y-%m-%d-%H-%M`.json
+scrapy crawl airbnb -o airbnb_`date +%Y-%m-%d-%H-%M`.json
+scrapy crawl wimdu -o wimdu_`date +%Y-%m-%d-%H-%M`.json
+scrapy crawl sejourning -o sejourning_`date +%Y-%m-%d-%H-%M`.json
+scrapy crawl cohebergement 
+scrapy crawl elouehebergement
 mysqldump -uroot -plifemaker1989 test ads > ads_`date +%Y-%m-%d-%H-%M`.sql
+zip ads_`date +%Y-%m-%d-%H-%M`.sql.zip ads_`date +%Y-%m-%d-%H-%M`.sql 
