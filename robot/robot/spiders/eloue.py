738bb720c907577e8269ba4e8f322a903748d35a
mohamed <mohamed.ibnalkadi@gmail.com>
2015-04-11 21:01:33 +0200

    eloue correction.

--- robot/robot/spiders/eloue.py
+++ robot/robot/spiders/eloue.py
@@ -23,36 +23,42 @@ class EloueBricoSpider(scrapy.Spider):
             try:
                 item['title'] = sel.xpath("@name").extract()[0]
             except:
-                print("scraping fails")
+                item['title'] = empty
             try:
                 item['media'] = "https:"+sel.xpath('div/div/a/img/@style').extract()[0].split(')')[0].split(':')[-1]
 
             except:
-                print("scraping fails")
+                item['media'] = empty
             try:
                 item['url'] = self.allowed_domains[0] + sel.xpath('div/div/a/@href').extract()[0]
             except:
-                print("scraping fails")
+                item['url'] = empty
             try:
                 item['description'] = sel.xpath('div/div[@class="info"]/p[@class="full_description"]/text()').extract()[0]
 
             except:
-                print("scraping fails")
+                item['description'] = empty
             try:
-                item['location'] = sel.xpath('div/div[@class="info"]/p[@class="full_description"]/text()').extract()[0]
+                item['location'] = sel.xpath('div/div[@class="info"]/p/text()').extract()[0]
             except:
-                print("scraping fails")
+                item['location'] = empty
             
-            item['latitude'] = sel.xpath("@locationx").extract()[0]
-            item['longitude'] = sel.xpath("@locationy").extract()[0]
-            
+            try:
+                item['latitude'] = sel.xpath("@locationx").extract()[0]
+            except:
+                item['latitude'] = empty
+            try:
+                item['longitude'] = sel.xpath("@locationy").extract()[0]
+            except:
+                item['longitude'] = empty
             try:
                 price = sel.xpath('div/div/span[@class="badge price"]/text()').extract()[0].split('/')
 
                 item['price'] = price[0].strip(' ').encode('utf-8').strip('€')
                 item['period'] = price[1]
             except:
-                print("scraping fails")
+                item['price'] = empty
+                item['period'] = empty
             
             yield item
 
