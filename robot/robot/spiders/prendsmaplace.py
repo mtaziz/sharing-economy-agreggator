ced2d635fc6a8272dad6dd78d4812a45ed736f96
mohamed <mohamed.ibnalkadi@gmail.com>
2015-05-14 22:10:37 +0200

    [robot]: prendsmaplace spider correction.

--- robot/robot/spiders/prendsmaplace.py
+++ robot/robot/spiders/prendsmaplace.py
@@ -2,29 +2,32 @@
 import scrapy 
 from robot.items import AdItem
 import datetime
+from robot.country import France
 
-class DrivySpider(scrapy.Spider):
+class PrendsmaplaceSpider(scrapy.Spider):
     name = "prendsmaplace"
     category = "parking"
     subcategory = "parking"
     allowed_domains = ["http://www.prendsmaplace.fr"]
-    # scrap zilok by categories
-    start_urls = list(map(lambda x: "http://www.prendsmaplace.fr/page/%s/?s&geo-radius=100&geo-lat&geo-lng&categories=0&locations=0&dir-search=yes"%str(x), range(1,25)))
+    France = France()
+    cities = France.cities
 
+    start_urls = list(map(lambda x: "http://www.prendsmaplace.fr/page/%s/?s&geo-radius=100&geo-lat&geo-lng&categories=0&locations=0&dir-search=yes"%str(x), range(1,25)))
 
     def parse(self, response):
         for sel in response.xpath('//ul[@class="items"]/li'):
             item = AdItem()
-            empty = "unknown"
+            empty = ""
             item['source'] = self.name
             item['category'] = self.category
             item['subcategory'] = self.subcategory
 
             try:
                 item['title'] = sel.xpath('div[@class="description"]/h2/text()').extract()[0]
-
+                item['location'] = self.France.city_from_title(item['title'])
             except:
                 item['title'] =empty
+                item['location'] = empty
             try:
                 item['media'] = sel.xpath('div[@class="thumbnail"]/img/@src').extract()[0]
             except:
@@ -38,11 +41,10 @@ class DrivySpider(scrapy.Spider):
             except:
                 item['description'] =empty
             
-            item['location'] =empty            
             item['latitude'] = empty
             item['longitude'] = empty           
             item['price'] = empty
-            
+            item['currency'] = empty
             item['period'] = empty
          
             yield item
