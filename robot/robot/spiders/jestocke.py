0e171ac82655166ee77a1600037f68366d4516ba
mohamed <mohamed.ibnalkadi@gmail.com>
2015-05-16 20:19:59 +0200

    [robot]: jetoscke spider corrected.

--- robot/robot/spiders/jestocke.py
+++ robot/robot/spiders/jestocke.py
@@ -1,18 +1,20 @@
 #-*- encoding:utf8 -*-
 import scrapy 
 from robot.items import AdItem
+from robot.country import France
 
 class JestockeSpider(scrapy.Spider):
 	name = "jestocke"
 	category = "storing"
 	subcategory = "space"
 	allowed_domains = ["http://www.jestocke.fr"]
-	# scrap boaterfly by pages
-	start_urls = list(map(lambda x: "https://www.jestocke.com/location/recherche/"+str(x), range(51)))
-
+	France = France()
+	cities = France.cities
+	start_urls_0 = list(map(lambda x: "https://www.jestocke.com/s/"+str(x), cities))
+	start_urls = [url+"?"+"page="+str(x) for url in start_urls_0 for x in range(10)]
 
 	def parse(self, response):
-		for sel in response.xpath('//article'):
+		for sel in response.xpath('//article[@id]'):
 			item = AdItem()
 		 	empty = ""
 			item['source'] = self.name
@@ -20,42 +22,38 @@ class JestockeSpider(scrapy.Spider):
 			item['subcategory'] = self.subcategory
 
 			try:
-				item['title'] = sel.xpath('div[2]/h2/a/text()').extract()[0]  
+				item['title'] = sel.xpath('a/div[3]/div/h3/text()').extract()[0].strip(' \n')
 			except:
 				item['title'] = empty
 			try:
-				item['media'] = self.allowed_domains[0] + sel.xpath('div[1]/img/@src').extract()[0]
+				item['media'] = self.allowed_domains[0] + sel.xpath('a/div/img/@src').extract()[0]
 			except:
 				item['media'] = empty
 			try:
-				item['url'] = self.allowed_domains[0] + sel.xpath('div[2]/h2/a/@href').extract()[0]
+				item['url'] = self.allowed_domains[0] + sel.xpath('a/@href').extract()[0]
 			except:
 				item['url'] = empty
 			try:
-				item['description'] = sel.xpath('div[2]/p/text()').extract()[0]
+				item['description'] = sel.xpath('a/div[3]/p/strong/text()').extract()[0]
 			except:
 				item['description'] = empty
 			try:
-				item['location']  = sel.xpath('div[2]/p[2]/text()').extract()[0]
+				item['location']  = sel.xpath('a/div[3]/div/h3/text()[3]').extract()[0]
 			except:
 				item['location'] = empty
+			
+			item['latitude'] = empty
+			item['longitude'] = empty
+			
 			try:
-				item['latitude']  = sel.xpath('div[2]/p[3]/a/span[@class="latitude"]/text()').extract()[0]
-			except:
-				item['latitude'] = empty
-			try:
-				item['longitude'] = sel.xpath('div[2]/p[3]/a/span[@class="longitude"]/text()').extract()[0]
-			except:
-				item['longitude'] = empty
-			try:
-				item['price'] = sel.xpath('div[3]/div/div/span[@class="price-figure"]/text()').extract()[0].encode('utf-8').strip('€')
+				item['price'] = sel.xpath('a/div[@class="ad-item_price"]/div/div/text()').extract()[0].encode('utf-8').strip('€')
 				item['currency'] = "€"
 			except:
 				item['price'] = empty
 				item['currency'] = empty
 
 			try:	
-				item['period'] = sel.xpath('div[3]/div/div/text()').extract()[0]
+				item['period'] = sel.xpath('a/div[@class="ad-item_price"]/div/div[2]/text()').extract()[0]
 			except:
 				item['period'] = empty 
 			yield item
