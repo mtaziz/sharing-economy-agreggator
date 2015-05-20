a17b91e250fc92c6563b8304bfa9f9f53b3a0a62
mohamed <mohamed.ibnalkadi@gmail.com>
2015-05-20 19:30:03 +0200

    [robot]: costockage spider.

---
+++ robot/robot/spiders/costockage.py
@@ -0,0 +1,59 @@
+#-*- encoding:utf8 -*-
+import scrapy 
+from robot.items import AdItem
+from robot.country import France
+
+class CostockageSpider(scrapy.Spider):
+	name = "costockage"
+	category = "storing"
+	subcategory = "space"
+	allowed_domains = ["https://www.costockage.fr"]
+	France = France()
+	cities = France.cities
+	start_urls_0 = list(map(lambda x: "https://www.costockage.fr/garde-meuble/%s-5&plus-proche=10"%str(x), cities))
+	start_urls = [url+"&"+"page="+str(x) for url in start_urls_0 for x in range(10)]
+
+	def parse(self, response):
+		for sel in response.xpath('//div[@itemtype="http://schema.org/Product"]'):
+			item = AdItem()
+		 	empty = ""
+			item['source'] = self.name
+			item['category'] = self.category
+			item['subcategory'] = self.subcategory
+
+			try:
+				item['title'] = sel.xpath('@title').extract()[0]
+			except:
+				item['title'] = empty
+			try:
+				item['media'] = sel.xpath('div[1]/div[@class="customer_name_search"]/p/img/@src').extract()[0]
+			except:
+				item['media'] = empty
+			try:
+				item['url'] = sel.xpath('@id').extract()[0]
+			except:
+				item['url'] = empty
+			try:
+				item['description'] = sel.xpath('div[1]/div[@class="address"]/text()[2]').extract()[0]
+			except:
+				item['description'] = empty
+			try:
+				item['location']  = sel.xpath('div[1]/div[@class="address"]/a/text()').extract()[0]
+			except:
+				item['location'] = empty
+			
+			item['latitude'] = empty
+			item['longitude'] = empty
+			
+			try:
+				item['price'] = sel.xpath('div[3]/div[@class="price_div"]/div[@class="new_price"]/b/text()').extract()[0].encode('utf-8').split('€')[0]
+				item['currency'] = "€"
+			except:
+				item['price'] = empty
+				item['currency'] = empty
+
+			try:	
+				item['period'] = sel.xpath('div[3]/div[@class="price_div"]/div[@class="new_price"]/text()[2]').extract()[0].strip('/')
+			except:
+				item['period'] = empty 
+			yield item
--- robot/scrape.sh
+++ robot/scrape.sh
@@ -2,38 +2,44 @@
 
 source $WORKON_HOME/scrapper/bin/activate
 cd /home/mohamed/side_projects/alterre.org/robot
-scrapy crawl drivy 
-scrapy crawl housetrip
-scrapy crawl chambrealouer
-scrapy crawl jelouemoncampingcar
+
+scrapy crawl airbnb 
+scrapy crawl plantezcheznous
+scrapy crawl bandbike
 scrapy crawl boaterfly
-scrapy crawl jestocke
-scrapy crawl ouicar
-scrapy crawl sailsharing 
-scrapy crawl ouistock
+scrapy crawl prendsmaplace
+scrapy crawl pretersonjardin
+scrapy crawl chambrealouer
+scrapy crawl cavientdujardin
+scrapy crawl cohebergement 
+scrapy crawl cookening
+scrapy crawl costockage
+scrapy crawl drivy 
 scrapy crawl eloue
 scrapy crawl elouesport
 scrapy crawl elouemoto
 scrapy crawl elouevelo
-scrapy crawl supermarmite 
+scrapy crawl elouehebergement
+
+scrapy crawl jelouemoncampingcar
+scrapy crawl jestocke
+scrapy crawl housetrip
 scrapy crawl meetup 
+scrapy crawl meetnsport
+scrapy crawl monsieurparking
+scrapy crawl parkadom
+
+scrapy crawl ouicar
+scrapy crawl ouistock
+scrapy crawl owlcamp
+
+scrapy crawl sailsharing 
+scrapy crawl samboat
+scrapy crawl sejourning 
+scrapy crawl supermarmite 
 scrapy crawl zealguide
+
+scrapy crawl videdressing
 scrapy crawl zilok 
 scrapy crawl zilokmanutention
-scrapy crawl airbnb 
 scrapy crawl wimdu 
-scrapy crawl sejourning 
-scrapy crawl cohebergement 
-scrapy crawl elouehebergement
-scrapy crawl owlcamp
-scrapy crawl bandbike
-scrapy crawl samboat
-scrapy crawl cookening
-scrapy crawl plantezcheznous
-scrapy crawl pretersonjardin
-scrapy crawl prendsmaplace
-scrapy crawl cavientdujardin
-scrapy crawl videdressing
-scrapy crawl meetnsport
-scrapy crawl parkadom
-scrapy crawl monsieurparking
