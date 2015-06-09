1b15ed3d564b3a966c70844d159b7678886c35ea
mohamed <mohamed.ibnalkadi@gmail.com>
2015-06-09 23:51:27 +0200

    [robot]: retrieve jestocke data from given API.

--- robot/robot/spiders/jestocke.py
+++ robot/robot/spiders/jestocke.py
@@ -2,58 +2,65 @@
 import scrapy 
 from robot.items import AdItem
 from robot.country import France
+import json
 
 class JestockeSpider(scrapy.Spider):
 	name = "jestocke"
 	category = "storing"
 	subcategory = "space"
 	allowed_domains = ["http://www.jestocke.fr"]
-	France = France()
-	cities = France.cities
-	start_urls_0 = list(map(lambda x: "https://www.jestocke.com/s/"+str(x), cities))
-	start_urls = [url+"?"+"page="+str(x) for url in start_urls_0 for x in range(10)]
+	start_urls = ["https://www.jestocke.com/api/stockage.json?api_key=c5c2a77d-92ce-4126-a265-614e8b510d93&hitsPerPage=1000"]
 
 	def parse(self, response):
-		for sel in response.xpath('//article[@id]'):
+		jsonresponse = json.loads(response.body_as_unicode())
+		#print jsonresponse
+		results = jsonresponse["hits"]
+		for sel in results:
 			item = AdItem()
 		 	empty = ""
 			item['source'] = self.name
 			item['category'] = self.category
 			item['subcategory'] = self.subcategory
-
+			
 			try:
-				item['title'] = sel.xpath('a/div[3]/div/h3/text()').extract()[0].strip(' \n')
+				item['title'] = sel['title']
 			except:
 				item['title'] = empty
 			try:
-				item['media'] = self.allowed_domains[0] + sel.xpath('a/div/img/@src').extract()[0]
+				item['media'] = sel["pictures"][0]
 			except:
 				item['media'] = empty
 			try:
-				item['url'] = self.allowed_domains[0] + sel.xpath('a/@href').extract()[0]
+				item['url'] = sel['url']
 			except:
 				item['url'] = empty
 			try:
-				item['description'] = sel.xpath('a/div[3]/p/strong/text()').extract()[0]
+				item['description'] = sel['comment']
 			except:
 				item['description'] = empty
 			try:
-				item['location']  = sel.xpath('a/div[3]/div/h3/text()[3]').extract()[0]
+				item['location']  = sel['address']
 			except:
 				item['location'] = empty
 			
-			item['latitude'] = empty
-			item['longitude'] = empty
+			try:
+				item['latitude'] = sel['lat']
+			except:
+				item['latitude'] = empty
 			
 			try:
-				item['price'] = sel.xpath('a/div[@class="ad-item_price"]/div/div/text()').extract()[0].encode('utf-8').split('€')[0]
+				item['longitude'] = sel['lng']
+			except:
+				item['longitude'] = empty
+			
+			try:
+				item['price'] = sel["unit_month_price_with_fee"]
 				item['currency'] = "€"
+				item['period'] = "par mois"
 			except:
 				item['price'] = empty
 				item['currency'] = empty
-
-			try:	
-				item['period'] = sel.xpath('a/div[@class="ad-item_price"]/div/div[2]/text()').extract()[0]
-			except:
-				item['period'] = empty 
+				item['period'] = empty
+				
 			yield item
+		
