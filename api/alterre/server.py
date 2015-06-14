9aa28cfa73fb322644e1cf644334d9c74f674efc
mohamed <mohamed.ibnalkadi@gmail.com>
2015-06-14 10:27:29 +0200

    [api]: filter ads from a point (lat, lon) for a raduis by km.

--- api/alterre/server.py
+++ api/alterre/server.py
@@ -7,12 +7,20 @@ import ast
 from hashlib import md5
 from datetime import datetime
 import xml.etree.ElementTree as ET
+from haversine import haversine
+import re
 
 def get_guid(item):
     """Generates an unique identifier for a given item."""
     # hash based solely in the description field
     return md5(item).hexdigest()
 
+def valid_float(element):
+	if element is None:
+		return False
+	if re.match("^\d+?\.\d+?$", element) is None:
+		return False
+	return True
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.write("Here we are integrating the widget.")
@@ -25,75 +33,55 @@ class AdsHandler(tornado.web.RequestHandler):
 		rows = None
 		ads = dict()
 		zone = self.get_argument('zone', None)
-		format = self.get_argument('format', None)
-		print zone, format
-		if zone is not None:
-			rows  = db.query("""
-								select guid, location, latitude, longitude, subcategory, category, price, title, description, url, media from ads where location like concat(%s, '%%')
-							""", (zone))
-					
-			total = db.get("""select count(guid) from ads where location like concat(%s, '%%')
-							""", (zone))
-			ads["total"] = total["count(guid)"]	
-			ads["zone"] = zone
-		else:
-			rows  = db.query("""
-								select * from ads limit 1000;
-							""")
-			total = db.get("select count(guid) from ads")
-			ads["total"] = total["count(guid)"]	
+		latitude =self.get_argument('lat', None)
+		longitude = self.get_argument('lon', None)
+		radius = self.get_argument('radius', None)
+		print radius
+		self.set_header("Content-Type", "application/json")
+	
+		if latitude is not None and longitude is not None and radius is not None:
+			results = []
+			rows  = db.query("""select location, latitude, longitude, subcategory, category, price, title, description, url, media from ads""")
+			correct_rows = filter(lambda x:valid_float(x["latitude"]) == True, rows)
+			center = (float(latitude), float(longitude))
 			
-			ads["zone"] = "France"
-		 
-		db.close()
-		self.set_header("Content-Type", "application/xml")
-		if format == 'xml':
-			data = ET.Element('data')
-			for row in rows:
-				guid = row["guid"]
-				_location = row["location"]
-				_lat = row["latitude"]
-				_lng = row["longitude"]
-				_subcategory = row["subcategory"]
-				_category = row["category"]
-				_price = row["price"]
-				_title = row["title"]
-				_description = row["description"]
-				_url = row["url"]
-				_media = row["media"]
-
-				ad = ET.SubElement(data, 'ad')
-				ad.attrib = {"id": guid}
-				location = ET.SubElement(ad, 'location')
-				print(_location)
-				location.attrib = {"city": _location}
-
-				subcategory = ET.SubElement(ad, 'subcategory')
-				subcategory.text = _subcategory
-				category = ET.SubElement(ad, 'category')
-				category.text = _category
-				
-				price = ET.SubElement(ad, 'price')
-				price.text = _price
-
-				title = ET.SubElement(ad, 'title')
-				title.text = _title
-				description = ET.SubElement(ad, 'description')
-				description.text = _description
-
-				url = ET.SubElement(ad, 'url')
-				url.text = _url
-				
-				media = ET.SubElement(ad, 'media')
-				media.text = _media
-				
-			result = str(ET.tostring(data))	
-			print result
-			self.write(result)
-		else:	
+			for row in correct_rows:
+				target = (float(row["latitude"]), float(row["longitude"]))
+				if haversine(center, target) <= float(radius):
+					print haversine(center, target), radius
+					results.append(row)
+					print len(results)
 			
-			ads["ads"] = rows
+			ads["ads"] = results
+			ads["count"] = len(results)
+
 			self.write(json.dumps(ads))
+			db.close()
+	
+		else:
+			if zone is not None:
+				rows  = db.query("""
+									select location, latitude, longitude, subcategory, category, price, title, description, url, media from ads where location like concat(%s, '%%')
+								""", (zone))
+						
+				total = db.get("""select count(guid) from ads where location like concat(%s, '%%')
+								""", (zone))
+				ads["ads"] = rows
+				ads["total"] = total["count(guid)"]	
+				ads["zone"] = zone
+			else:
+				rows  = db.query("""
+									select location, latitude, longitude, subcategory, category, price, title, description, url, media  from ads limit 1000;
+								""")
+				total = db.get("select count(guid) from ads")
+				ads["ads"] = rows
+				ads["total"] = total["count(guid)"]	
+				
+				ads["zone"] = "France"
+			self.write(json.dumps(ads))
+			db.close()
+			 
+		self.set_header("Content-Type", "application/json")
 
 class HousingAdsHandler(tornado.web.RequestHandler):
 	def get(self):
