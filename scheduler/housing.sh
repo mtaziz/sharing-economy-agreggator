d328ae00202ac1fd90025ce5d06113da5e2d4189
mohamed <mohamed.ibnalkadi@gmail.com>
2015-06-16 08:03:20 +0200

    [scheduler]: housing cronjob.

---
+++ scheduler/housing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo $(date)  > /tmp/test.log
+echo "start scraping housing at" $(date)> /tmp/test.log
+cd /root/alterre/alterre.org/robot
+/usr/local/bin/scrapy crawl sejourning
+/usr/local/bin/scrapy crawl housetrip
+/usr/local/bin/scrapy crawl airbnb
+/usr/local/bin/scrapy crawl chambrealouer
+
+echo "finish scraping housing at" $(date)> /tmp/test.log
