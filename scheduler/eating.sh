ef87eab796c0e2b14f5b85e876a0b51c25479409
mohamed <mohamed.ibnalkadi@gmail.com>
2015-06-26 20:22:54 +0200

    [scheduler]: eating scraping schedule.

--- scheduler/eating.sh
+++ scheduler/eating.sh
@@ -2,10 +2,7 @@
 echo $(date)  > /tmp/test.log
 echo "start scraping eating at" $(date)> /tmp/test.log
 cd /root/alterre/alterre.org/robot
-/usr/local/bin/scrapy crawl pretersonjardin
-/usr/local/bin/scrapy crawl plantezcheznous
 /usr/local/bin/scrapy crawl vizeat
-/usr/local/bin/scrapy crawl cavientdujardin
-/usr/local/bin/scrapy crawl supermarmite
-
+/usr/local/bin/scrapy crawl cookening
+/usr/local/bin/scrapy crawl plantezcheznous
 echo "finish scraping eating at" $(date)> /tmp/test.log
