b03b7492a7d3cae9e8738c23081228b37fac39a0
mohamed <mohamed.ibnalkadi@gmail.com>
2015-07-04 11:02:04 +0200

    [robot]: boat/samboat more data scraped.

--- robot/robot/spiders/samboat.py
+++ robot/robot/spiders/samboat.py
@@ -3,19 +3,20 @@ import scrapy
 from robot.items import AdItem
 import datetime
 
-class SailsharingSpider(scrapy.Spider):
+class SamboatSpider(scrapy.Spider):
     name = "samboat"
     category = "leisure"
     subcategory = "boat"
     allowed_domains = ["https://www.samboat.fr"]
     # scrap zilok by categories
-    start_urls = ["https://www.samboat.fr/location-bateau/"]
+    start_urls = list(map(lambda x:"https://www.samboat.fr/location-bateau/?&page="+str(x), range(1,31)))
 
 
     def parse(self, response):
+
         for sel in response.xpath('//div[@id="liste_bateaux"]/div'):
             item = AdItem()
-            empty = 'unknown'
+            empty = ''
             item['source'] = self.name
             item['category'] = self.category
             item['subcategory'] = self.subcategory
@@ -25,17 +26,27 @@ class SailsharingSpider(scrapy.Spider):
 
             except:
                 item['title'] = empty
+            
             try:
                 item['media'] = sel.xpath('div/div/a/img/@src').extract()[0]
             except:
                 item['media'] = empty
+            
             try:
                 item['url'] = sel.xpath('div/div/a/@href').extract()[0]
             except:
                 item['url'] = empty
-
-            item['description'] = empty
-            item['location'] = empty
+            
+            try:
+                item['description'] = sel.xpath('div[2]/div[@class="row"]/div/div[@class="list-capacite"]/text()').extract()[0].strip('\n')
+                
+            except:
+                item['description'] = empty    
+            
+            try:
+                item['location'] = sel.xpath('div[2]/div[@class="row"]/div/a/span[@class="location"]/text()').extract()[0].strip('\t')
+            except:
+                item['location'] = empty
             
             try:          
                 item['latitude'] = sel.xpath('div/input[@class="annonce_lat"]/@value').extract()[0]
@@ -46,10 +57,13 @@ class SailsharingSpider(scrapy.Spider):
                 item['longitude'] = sel.xpath('div/input[@class="annonce_ltd"]/@value').extract()[0]
             except:
                 item['longitude'] = empty
+            
             try:
                 item['price'] = sel.xpath('div/input[@class="annonce_px_jour"]/@value').extract()[0]
+                item['currency'] = '€'
             except:
                 item['price'] = empty
+                item['currency'] = empty
             
             try:
                 item['period'] = sel.xpath('div[3]/span/text()').extract()[0]
