6700f24bc809fc85e8773fb8c0243be149b2b165
mohamed <mohamed.ibnalkadi@gmail.com>
2015-07-05 21:08:39 +0200

    [scheduler]: leisure boat.

--- scheduler/leisure.sh
+++ scheduler/leisure.sh
@@ -4,5 +4,7 @@ echo "start scraping leisure at" $(date)> /tmp/test.log
 cd /root/alterre/alterre.org/robot
 /usr/local/bin/scrapy crawl elouesport
 /usr/local/bin/scrapy crawl boaterfly
+/usr/local/bin/scrapy crawl clickandboat
+/usr/local/bin/scrapy crawl samboat
 
 echo "finish scraping leisure at" $(date)> /tmp/test.log
