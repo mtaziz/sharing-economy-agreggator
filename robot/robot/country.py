b5ef36a7502562181e1b659cde1c0dce7dbca192
mohamed <mohamed.ibnalkadi@gmail.com>
2015-07-08 19:59:57 +0200

    [robot]: adding 300 big france cities.

--- robot/robot/country.py
+++ robot/robot/country.py
@@ -4,26 +4,25 @@ class France:
 	def __init__(self):
 
 		self.cities = [
-					"Biarritz","Paris","Amiens","Nancy",
-					"Rouen","Caen","Evreux","Saint Lo",
-					"Rennes","Quimper","Morlaix","Vannes",
-					"Strasbourg","Nantes","Clermont Ferrand",
-					"Bordeaux","Dax","Chambery",
-					"Poitiers","Perpignan","Nimes","Montpellier",
-					"Marseille","Nice","Lyon","Toulouse","Limoges",
-					"Besancon","Troyes","Orléans","Le mans","Gap","Millau",
-					"Brives","Reims","Avallon","Le Puy en Velay","Aurillac",
-					"Privas","Valence","Agen","Saint Brieuc","Cherbourg",
-					"Charleville","Nevers","Angers","Pau",
-					"Lille","calais","Moulins","Chateauroux","Chartres",
-					"Angouleme","Chateaubriant","Pontivy","chaumont","Mulhouse",
-					"Mende","Saint flour","Nyons","Digne","Draguignan","Grenoble",
-					"Auch","Foix","Bagnères de Luchon","Castres","Albi","Cahors",
-					"Rodez","La rochelle","Chalon sur Saone","Dijon","Manosque","Frejus",
-					"Annecy","Thonon les bains","Albertville","Briancon",
-					"Carcasonne","Narbonne","Mimizan","Arcachon","Bergerac","Saintes", 
-					"Tours","Chateau-Thierry","Saint Malo","Loudeac","Carhaix","brest",
-					"Laval","Le Mans","Dreux","Charleville"
+						"Paris","Marseille","Lyon","Toulouse","Nice","Nantes","Strasbourg","Montpellier","Bordeaux","Lille","Rennes",
+						"Reims","Le Havre","Saint-Étienne","Toulon","Grenoble","Dijon","Angers","Nîmes","Villeurbanne","Saint-Denis",
+						"Le Mans","Clermont-Ferrand","Aix-en-Provence","Brest","Limoges","Tours","Amiens","Perpignan","Metz","Boulogne-Billancourt","Besançon","Orléans","Rouen","Mulhouse","Caen","Saint-Denis","Nancy","Argenteuil","Saint-Paul","Montreuil","Roubaix","Tourcoing","Dunkerque","Nanterre","Créteil","Avignon","Vitry-sur-Seine","Poitiers",
+						"Courbevoie","Fort-de-France","Versailles","Colombes","Asnières-sur-Seine","Aulnay-sous-Bois","Saint-Pierre",
+						"Rueil-Malmaison","Pau","Aubervilliers","Champigny-sur-Marne","Le Tampon","Antibes","Saint-Maur-des-Fossés",
+						"La Rochelle","Cannes","Béziers","Calais","Saint-Nazaire","Colmar","Drancy","Bourges","Mérignac","Ajaccio",
+						"Issy-les-Moulineaux","Levallois-Perret","La Seyne-sur-Mer","Quimper","Noisy-le-Grand","Valence","Villeneuve-d'Ascq","Neuilly-sur-Seine","Antony","Vénissieux","Cergy","Troyes","Clichy","Pessac","Les Abymes","Ivry-sur-Seine",
+						"Chambéry","Lorient","Niort","Sarcelles","Montauban","Villejuif","Saint-Quentin","Hyères","Cayenne","Épinay-sur-Seine","Saint-André","Beauvais","Maisons-Alfort","Cholet","Meaux","Chelles","Pantin","Fontenay-sous-Bois","La Roche-sur-Yon","Bondy","Vannes","Saint-Louis","Fréjus","Arles","Clamart","Évry","Le Blanc-Mesnil","Narbonne","Sartrouville","Grasse","Annecy","Laval","Belfort","Vincennes","Charleville-Mézières","Évreux",
+						"Sevran","Albi","Montrouge","Bobigny","Martigues","Saint-Ouen","Brive-la-Gaillarde","Suresnes","Carcassonne","Cagnes-sur-Mer","Corbeil-Essonnes","Saint-Brieuc","Blois","Bayonne","Aubagne","Châlons-en-Champagne","Meudon","Châteauroux","Saint-Malo","Chalon-sur-Saône","Sète","Puteaux","Alfortville","Salon-de-Provence","Massy","Mantes-la-Jolie","Bastia","Vaulx-en-Velin","Saint-Herblain","Le Cannet","Valenciennes","Istres",
+						"Gennevilliers","Boulogne-sur-Mer","Livry-Gargan","Saint-Priest","Rosny-sous-Bois","Caluire-et-Cuire","Angoulême",
+						"Douai","Tarbes","Wattrelos","Castres","Choisy-le-Roi","Talence","Thionville","Arras","Alès","Garges-lès-Gonesse",
+						"Gap","Saint-Laurent-du-Maroni","Melun","Bourg-en-Bresse","Noisy-le-Sec","Compiègne","La Courneuve","Le Lamentin",
+						"Marcq-en-Barœul","Saint-Germain-en-Laye","Rezé","Bron","Anglet","Gagny","Chartres","Bagneux","Saint-Martin-d'Hères","Montluçon","Pontault-Combault","Poissy","Draguignan","Joué-lès-Tours","Savigny-sur-Orge","Cherbourg-Octeville","Saint-Joseph","Le Port","Colomiers","Saint-Martin","Villefranche-sur-Saône","Stains","Saint-Benoît",
+						"Échirolles","Villepinte","Roanne","Montélimar","Saint-Chamond","Nevers","Moulins sur allier","Conflans-Sainte-Honorine","Auxerre","Sainte-Geneviève-des-Bois","Châtillon","Bagnolet","Vitrolles","Thonon-les-Bains","Neuilly-sur-Marne","Haguenau","Marignane","Saint-Raphaël","Tremblay-en-France","La Ciotat","Six-Fours-les-Plages","Creil","Agen","Romans-sur-Isère","Montigny-le-Bretonneux","Le Perreux-sur-Marne","Franconville","Annemasse",
+						"Villeneuve-Saint-Georges","Saint-Leu","Mâcon","Cambrai","Lens","Houilles","Épinal","Châtenay-Malabry","Schiltigheim","Sainte-Marie","Liévin","Châtellerault","Meyzieu","Goussainville","Viry-Châtillon","Dreux",
+						"L'Haÿ-les-Roses","Plaisir","Mont-de-Marsan","Maubeuge","Nogent-sur-Marne","Les Mureaux","Clichy-sous-Bois","La Possession","Dieppe","Chatou","Vandœuvre-lès-Nancy","Malakoff","Palaiseau","Pontoise","Charenton-le-Pont","Rillieux-la-Pape","Baie-Mahault","Saint-Laurent-du-Var","Athis-Mons","Périgueux","Trappes","Vienne","Sotteville-lès-Rouen",
+						"Soissons","Saint-Étienne-du-Rouvray","Vallauris","Aurillac","Saumur","Vierzon","Alençon","Montbéliard","Biarritz",
+						"Vichy","Saint-Dizier","Le Grand-Quevilly","Bruay-la-Buissière","Le Creusot","Orly","saint enimie","Meyrueis",
+						"Mende","Saint chely","Millau","Rodez","auch","Porto Vecchio","Piana","Galeria","Saint Laurent du verdon","Vallon pont d'arc"
 					]
 
 	def add_city(self, city):
