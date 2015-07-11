935b4548af8aeb92dbbf5aebc1ab522d24397a97
mohamed <mohamed.ibnalkadi@gmail.com>
2015-07-11 21:16:48 +0200

    [scheduler]: eloue velo spider.

--- scheduler/moving.sh
+++ scheduler/moving.sh
@@ -7,5 +7,5 @@ cd /root/alterre/alterre.org/robot
 /usr/local/bin/scrapy crawl jelouemoncampingcar
 /usr/local/bin/scrapy crawl wikicampers
 /usr/local/bin/scrapy crawl bandbike
-
+/usr/local/bin/scrapy crawl elouevelo
 echo "finish scraping moving at" $(date)> /tmp/test.log
