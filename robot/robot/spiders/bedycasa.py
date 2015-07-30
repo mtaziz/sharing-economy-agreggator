6a0a9d4770f59485805e280ee25f3db8e97d346d
Mohamed IBN AL KADI <mohamed.ibnalkadi@gmail.com>
2015-07-30 11:45:07 +0000

    ["robot": ]bedycasa API generating access_token

---
+++ robot/robot/spiders/bedycasa.py
@@ -0,0 +1,14 @@
+#-*- coding:utf-8 -*-
+import requests
+import json
+#Bedycasa API documentation: http://www.bedycasa.com/api/doc/index.html
+#generate access_token: valid 3600s
+
+url = "https://www.bedycasa.com/oauth/v2/token"
+client_id = "21_48jkpjawhuec0ock0ocgkcco00ocww80c8ookw08wwo0ck0wg8"
+client_secret = "40h63xgptxwk4sgsw404g04c0ogoskww4ckoo04ok440c48wg"
+grant_type = "client_credentials"
+params = {"client_id":client_id, "client_secret":client_secret, "grant_type":grant_type}
+req_token = requests.get(url=url, params=params)
+result_token = json.loads(re_token.text)
+access_token = result_token["access_token"]
