d9abbadba058a7977a3b9d4a8398b565dae4d6ae
mohamed <mohamed.ibnalkadi@gmail.com>
2015-08-03 15:21:22 +0200

    [scheduler]: storing cronjob.

---
+++ scheduler/storing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo $(date)  > /tmp/test.log
+echo "start scraping storing at" $(date)> /tmp/test.log
+cd /root/alterre/alterre.org/robot
+/usr/local/bin/scrapy crawl jestocke
+/usr/local/bin/scrapy crawl ouistock
+/usr/local/bin/scrapy crawl costockage
+
+echo "finish scraping storing at" $(date)> /tmp/test.log
