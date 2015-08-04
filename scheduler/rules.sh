df91a180831b8e4ccf2685e58fd2ca139c5019f0
mohamed <mohamed.ibnalkadi@gmail.com>
2015-08-04 10:26:27 +0200

    [rules]: delete 'ads' containing 'stage '.

--- scheduler/rules.sh
+++ scheduler/rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo $(date)  > /tmp/test.log
-echo "start preprocessing stored data  at" $(date)> /tmp/test.log
-mysql --xml -uroot -plifemaker1989 alterrefront -e "UPDATE ads SET subcategory='sport' WHERE title LIKE '%velo%appart%';"
-
-echo "finish preprocessing stored data at" $(date)> /tmp/test.log
+echo $(date)  > /tmp/scraper.log
+echo "start preprocessing stored data  at" $(date)> /tmp/scraper.log
+mysql  -uroot -plifemaker1989 alterrefront -e "UPDATE ads SET subcategory='sport' WHERE title LIKE '%velo%appart%';"
+mysql -uroot -plifemaker1989 alterrefront -e "DELETE FROM ads WHERE title LIKE 'stage %';"
+echo "finish preprocessing stored data at" $(date)> /tmp/scraper.log
