bec0ece16bf346373580d7d9234ea5d5827748a4
mohamed <mohamed.ibnalkadi@outlook.com>
2015-08-04 11:48:53 +0000

    [robot]: bandbike scraping enhanced.

--- robot/robot/spiders/bandbike.py
+++ robot/robot/spiders/bandbike.py
@@ -2,20 +2,32 @@
 import scrapy 
 from robot.items import AdItem
 import datetime
-
+from robot.country import France
+import requests
+import json
 
 class BandbikeSpider(scrapy.Spider):
     name = "bandbike"
     category = "moving"
     subcategory = "velo"
     allowed_domains = ["http://bandbike.com/"]
-    # scrap zilok by categories
-    start_urls = list(map(lambda x: "http://bandbike.com/ad/search?terms=Paris+%s+(7500%s)&searchCityId=29617&categoryFacetId=" %(str(x),str(x)), range(1,20)))
+    start_urls = []    
+    France = France()
+    cities = France.cities
+    for city in cities:
+    	url = "http://bandbike.com/ref/city/"+ city
+	req = requests.get(url=url)
+        res = json.loads(req.text)
+	for r in res:
+	    url = "http://bandbike.com/ad/search?terms=%s+(%s)&searchCityId=%s"%(r['name'],r['zipcode'], r['id'])
+	    urls = [url+"&currentPage="+str(x) for x in xrange(1,10)]
+	    start_urls += urls 
     
+    #print start_urls
     def parse(self, response):
         for sel in response.xpath('//div[@class="row"]'):
 			item = AdItem()
-			empty = "unknown"
+			empty = ""
 			item['source'] = self.name
 			item['category'] = self.category
 			item['subcategory'] = self.subcategory
@@ -34,17 +46,19 @@ class BandbikeSpider(scrapy.Spider):
 			except:
 				item['url'] = empty
 			try:
-				item['description'] = sel.xpath('div/div/div/h4/text()').extract()[0]
+				item['description'] = sel.xpath('div/div/div/div/div/h5/text()').extract()[0]
 
 			except:
 				item['description'] = empty
 			
-			item['location'] = empty
+			item['location'] = response.url.split('terms=')[1].split('+')[0]
+			item['postal_code'] = response.url.split('terms=')[1].split('+')[1].split(')')[0].strip('(')
+			item['evaluations'] = empty
 			item['latitude'] = empty
 			item['longitude'] = empty
 
 			try:
-				price = sel.xpath('div/div/div/div/div[3]/h5').extract()[0].split('/')
+				price = sel.xpath('div/div/div/div/div[3]/h5/text()').extract()[0].split('/')
 				item['price'] = price[0].strip(' ').encode('utf-8').strip('€')
 				item['period'] = price[1]
 				item['currency'] = "€"
