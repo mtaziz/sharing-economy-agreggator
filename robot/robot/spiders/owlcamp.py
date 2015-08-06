4675e5ae6645d46e2f1c1e8a44568f9908ea7a10
mohamed <mohamed.ibnalkadi@gmail.com>
2015-08-06 13:00:30 +0200

    :[robot]: owlcamp correction.

--- robot/robot/spiders/owlcamp.py
+++ robot/robot/spiders/owlcamp.py
@@ -2,13 +2,14 @@
 import scrapy 
 from robot.items import AdItem
 import datetime
-
+from robot.country import France
 class OwlcampSpider(scrapy.Spider):
 	name = "owlcamp"
 	category = "housing"
 	subcategory = "camping"
 	allowed_domains = ["http://owlcamp.com"]
-	# scrap zilok by categories
+	France = France()
+        geo = France.geo
 	start_urls = list(map(lambda x: "http://owlcamp.com/fre/gardens/all/page:%s" % str(x), range(2,15)))
 	start_urls.append("http://owlcamp.com/fre/gardens/all")
 
@@ -21,7 +22,7 @@ class OwlcampSpider(scrapy.Spider):
 			item['subcategory'] = self.subcategory
 
 			try:
-				item['title'] = sel.xpath('div[@class="garden-card__location"]/text()').extract()[0].strip(' ')
+				item['title'] = sel.xpath('div[@class="garden-card__location"]/text()').extract()[0].strip(' \n')
 			except:
 				item['title'] = empty
 			try:
@@ -33,16 +34,21 @@ class OwlcampSpider(scrapy.Spider):
 			except:
 				item['url'] = empty
 			try:
-				item['description'] = sel.xpath('div[@class="garden-card__location"]/text()').extract()[0].strip(' ')
+				item['description'] = sel.xpath('div[@class="garden-card__location"]/text()').extract()[0].strip(' \n')
 			except:
 				item['description'] = empty
 			try:
-				item['location'] = sel.xpath('div[@class="garden-card__location"]/text()').extract()[0].strip(' ')
+				item['location'] = sel.xpath('div[@class="garden-card__location"]/text()').extract()[0].strip(' \n')
 			except:
 				item['location'] = empty
-
-			item['latitude'] = empty
-			item['longitude'] = empty
+			try:
+				item['latitude'] = str(self.geo[item['location']]['lat'])
+			except:
+				item['latitude'] = empty
+			try:
+				item['longitude']= str(self.geo[item['location']]['lon'])
+			except:
+				item['longitude'] = empty
 
 			try:
 				price = sel.xpath('div[@class="garden-card__price"]/div/text()').extract()[0].strip(' ')
@@ -56,5 +62,6 @@ class OwlcampSpider(scrapy.Spider):
 				item['price'] = empty
 				item['period'] = empty
 				item['currency'] = empty
-
+			item['postal_code'] = empty
+			item['evaluations'] = empty
 			yield item
