8b0317a6f4125c23fd9451131a9eaa08771c41f6
mohamed <mohamed.ibnalkadi@gmail.com>
2015-08-06 15:52:05 +0200

    [robot]: sailsharing correction.

--- robot/robot/spiders/sailsharing.py
+++ robot/robot/spiders/sailsharing.py
@@ -2,6 +2,7 @@
 import scrapy 
 from robot.items import AdItem
 import datetime
+from robot.country import France
 
 class SailsharingSpider(scrapy.Spider):
     name = "sailsharing"
@@ -11,7 +12,8 @@ class SailsharingSpider(scrapy.Spider):
     # scrap zilok by categories
     start_urls = list(map(lambda x: "http://www.sailsharing.com/fr/location-bateau/search?page="+str(x), range(1,36)))
 
-
+    France = France()
+    geo = France.geo
     def parse(self, response):
         for sel in response.xpath('//div[@class="block"]'):
             item = AdItem()
@@ -38,13 +40,24 @@ class SailsharingSpider(scrapy.Spider):
 
             except:
                 item['description'] = empty
+	    try:
+                item['evaluations'] = sel.xpath('div/div[@class="boat-skipper"]/div[@class="nb-commentaires"]/span[@class="nb-com"]/text()').extract()[0].strip("\n ")
+
+            except:
+                item['evaluations'] = empty
+
             try:
-                item['location'] = sel.xpath('div/div/h4/strong/text()').extract()[0]
+                item['location'] = sel.xpath('div/div/h4/strong/text()').extract()[0].strip(' -')
             except:
                 item['location'] = empty
-                        
-            item['latitude'] = empty
-            item['longitude'] = empty
+            try:
+		item['latitude'] = self.geo[item['location']]['lat']
+	    except:    
+            	item['latitude'] = empty
+	    try:
+		item['longitude'] = self.geo[item['location']]['lon']
+	    except:
+            	item['longitude'] = empty
             
             try:
                 item['price'] = sel.xpath('div[@class="hosting-meta"]/div/span/strong/text()').extract()[0].encode('utf-8').strip('€')
@@ -57,5 +70,5 @@ class SailsharingSpider(scrapy.Spider):
                 item['period'] = sel.xpath('div[3]/span/text()').extract()[0]
             except:
                 item['period'] = empty
-
+	    item['postal_code'] = empty
             yield item
