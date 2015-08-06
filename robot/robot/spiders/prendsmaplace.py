e301d9b070ecf0385c0e4b072e5b4ab550383aec
mohamed <mohamed.ibnalkadi@gmail.com>
2015-08-06 14:04:29 +0200

    [robot]: prendsmaplace correction.

--- robot/robot/spiders/prendsmaplace.py
+++ robot/robot/spiders/prendsmaplace.py
@@ -3,15 +3,17 @@ import scrapy
 from robot.items import AdItem
 import datetime
 from robot.country import France
+import re
 
 class PrendsmaplaceSpider(scrapy.Spider):
     name = "prendsmaplace"
     category = "parking"
     subcategory = "parking"
     allowed_domains = ["http://www.prendsmaplace.fr"]
+    pattern = re.compile('\d{2}')
     France = France()
     cities = France.cities
-
+    geo = France.geo	
     start_urls = list(map(lambda x: "http://www.prendsmaplace.fr/page/%s/?s&geo-radius=100&geo-lat&geo-lng&categories=0&locations=0&dir-search=yes"%str(x), range(1,25)))
 
     def parse(self, response):
@@ -27,13 +29,18 @@ class PrendsmaplaceSpider(scrapy.Spider):
             except:
                 item['title'] =empty
             try:
-                item['location'] = self.France.city_from_title(item['title'])
+                item['location'] = item['title'].split(' (')[0].split(' ')[-1]
             except:
                 item['location'] = empty
             try:
                 item['media'] = sel.xpath('div[@class="thumbnail"]/img/@src').extract()[0]
             except:
                 item['media'] =empty
+	    try:
+                item['evaluations'] = sel.xpath('div[@class="thumbnail"]/div[@class="comment-count"]/text()').extract()[0]
+            except:
+                item['evaluations'] =empty
+
             try:
                 item['url'] = sel.xpath('div[@class="description"]/h3/a/@href').extract()[0]
             except:
@@ -42,11 +49,21 @@ class PrendsmaplaceSpider(scrapy.Spider):
                 item['description'] = sel.xpath('div[@class="description"]/text()[3]').extract()[0]
             except:
                 item['description'] =empty
-            
-            item['latitude'] = empty
+	    try:        
+            	item['latitude'] = self.geo[item['location']]['lat']
+	    except:
+		item['latitude'] = empty
+	    try:
+                item['longitude'] = self.geo[item['location']]['lon']
+            except:
+                item['longitude'] = empty
+
             item['longitude'] = empty           
             item['price'] = empty
             item['currency'] = empty
             item['period'] = empty
-         
+	    try:
+            	item['postal_code'] = re.search(self.pattern, item['title']).group()
+	    except:
+		item['postal_code'] = empty
             yield item
