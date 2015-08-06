ac325d9e78cf97ba2d86103b8e15d37673b49533
mohamed <mohamed.ibnalkadi@gmail.com>
2015-08-06 14:04:29 +0200

    [robot]: pretersonjardin correction.

--- robot/robot/spiders/pretersonjardin.py
+++ robot/robot/spiders/pretersonjardin.py
@@ -2,6 +2,7 @@
 import scrapy 
 from robot.items import AdItem
 import datetime
+from robot.country import France
 
 class HousetripSpider(scrapy.Spider):
 	name = "pretersonjardin"
@@ -10,6 +11,8 @@ class HousetripSpider(scrapy.Spider):
 	allowed_domains = ["http://www.pretersonjardin.com"]
 	pages = 18*range(1,1000)
 	start_urls = list(map(lambda x: "http://www.pretersonjardin.com/annonces/toutes-les-annonces/Page-%s.html"%str(x), range(1,100)))
+	France = France()
+	geo = France.geo
 
 	def parse(self, response):
 		for sel in response.xpath('//tr'):
@@ -20,7 +23,7 @@ class HousetripSpider(scrapy.Spider):
 			item['subcategory'] = self.subcategory
 
 			try:
-				item['title'] = sel.xpath('td[@id="colonne4"]/div[@id="title_ad"]/a/text()').extract()[0]
+				item['title'] = sel.xpath('td[@id="colonne4"]/div[@id="title_ad"]/a/text()').extract()[0].strip(' ').title()
 			except: 
 				item['title'] = empty
 
@@ -37,19 +40,25 @@ class HousetripSpider(scrapy.Spider):
 				item['description'] = empty
 
 			try:
-				item['location'] = sel.xpath('td[@id="colonne3"]/text()').extract()[0]
+				item['location'] = sel.xpath('td[@id="colonne3"]/text()').extract()[0].strip(' ').title()
 			except:
 				item['location'] = empty
 
-			
-			item['latitude'] = empty
-			item['longitude'] = empty
+			try:
+				item['latitude'] = self.geo[item['location']]['lat']
+			except:
+				item['latitude'] = empty
+			try:
+				item['longitude'] = self.geo[item['location']]['lon']
+			except:
+				item['longitude'] = empty
 			item['price'] = empty
 			item['currency'] = empty
-
+			
 			try:
 				item['period'] = sel.xpath('td[@id="colonne5"]/div/text()').extract()[0]
 			except:
 				item['period'] = empty
-			
+			item['postal_code'] = empty
+			item['evaluations'] = empty			
 			yield item
