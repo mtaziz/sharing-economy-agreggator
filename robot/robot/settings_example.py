b7f381342940735a0d765c4613ab307b766fbcfc
mohamed <mohamed.ibnalkadi@gmail.com>
2015-08-27 23:41:48 +0200

    [elasticsearch]: elasticsearc as a pipeline.

--- robot/robot/settings_example.py
+++ robot/robot/settings_example.py
@@ -1,3 +1,5 @@
+from scrapy import log
+
 # Scrapy settings for dirbot project
 
 SPIDER_MODULES = ['robot.spiders']
@@ -8,9 +10,19 @@ ITEM_PIPELINES = [
     'robot.pipelines.RequiredFieldsPipeline',
     'robot.pipelines.FilterWordsPipeline',
     'robot.pipelines.MySQLStorePipeline',
+    'scrapyelasticsearch.scrapyelasticsearch.ElasticSearchPipeline'
 ]
 
 MYSQL_HOST = 'localhost'
-MYSQL_DBNAME = 'test'
+MYSQL_DBNAME = 'alterrefront'
 MYSQL_USER = 'root'
 MYSQL_PASSWD = 'lifemaker1989'
+
+ELASTICSEARCH_SERVER = 'localhost' # If not 'localhost' prepend 'http://'
+ELASTICSEARCH_PORT = 9200 # If port 80 leave blank
+ELASTICSEARCH_USERNAME = ''
+ELASTICSEARCH_PASSWORD = ''
+ELASTICSEARCH_INDEX = 'robot'
+ELASTICSEARCH_TYPE = 'ads'
+ELASTICSEARCH_UNIQ_KEY = 'url'
+ELASTICSEARCH_LOG_LEVEL= log.DEBUG
